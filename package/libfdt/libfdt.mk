8bafc839065501e484b0878584d964af3c11fe47
Joe Watkins <krakjoe@php.net>
2016-12-08 11:24:28 +0000

    should use patched sources

--- package/libfdt/libfdt.mk
+++ package/libfdt/libfdt.mk
@@ -68,46 +68,46 @@ LIBFDT_INSTALL_STAGING = YES
 
 define LIBFDT_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CC) $(TARGET_CFLAGS) \
-		-I$(LINUX_DIR)/scripts/dtc/libfdt  -shared -o $(LINUX_DIR)/lib/libfdt.so \
-			$(LINUX_DIR)/scripts/dtc/libfdt/fdt.c \
-			$(LINUX_DIR)/scripts/dtc/libfdt/fdt_empty_tree.c \
-			$(LINUX_DIR)/scripts/dtc/libfdt/fdt_ro.c \
-			$(LINUX_DIR)/scripts/dtc/libfdt/fdt_rw.c \
-			$(LINUX_DIR)/scripts/dtc/libfdt/fdt_strerror.c \
-			$(LINUX_DIR)/scripts/dtc/libfdt/fdt_sw.c \
-			$(LINUX_DIR)/scripts/dtc/libfdt/fdt_wip.c;
+		-I$(@D)/scripts/dtc/libfdt  -shared -o $(@D)/libfdt.so \
+			$(@D)/scripts/dtc/libfdt/fdt.c \
+			$(@D)/scripts/dtc/libfdt/fdt_empty_tree.c \
+			$(@D)/scripts/dtc/libfdt/fdt_ro.c \
+			$(@D)/scripts/dtc/libfdt/fdt_rw.c \
+			$(@D)/scripts/dtc/libfdt/fdt_strerror.c \
+			$(@D)/scripts/dtc/libfdt/fdt_sw.c \
+			$(@D)/scripts/dtc/libfdt/fdt_wip.c;
 	$(TARGET_MAKE_ENV) $(TARGET_CC) $(TARGET_CFLAGS) \
-		-I$(LINUX_DIR)/scripts/dtc -I$(LINUX_DIR)/scripts/dtc/libfdt \
-			-L$(LINUX_DIR)/lib -o $(LINUX_DIR)/lib/fdtdump \
-			$(LINUX_DIR)/scripts/dtc/util.c \
-			$(LINUX_DIR)/scripts/dtc/fdtdump.c \
+		-I$(@D)/scripts/dtc -I$(@D)/scripts/dtc/libfdt \
+			-L$(@D) -o $(@D)/fdtdump \
+			$(@D)/scripts/dtc/util.c \
+			$(@D)/scripts/dtc/fdtdump.c \
 		-lfdt;
 	$(TARGET_MAKE_ENV) $(TARGET_CC) $(TARGET_CFLAGS) \
-		-I$(LINUX_DIR)/scripts/dtc -I$(LINUX_DIR)/scripts/dtc/libfdt \
-			-L$(LINUX_DIR)/lib -o $(LINUX_DIR)/lib/fdtput \
-			$(LINUX_DIR)/scripts/dtc/util.c \
-			$(LINUX_DIR)/scripts/dtc/fdtput.c \
+		-I$(@D)/scripts/dtc -I$(@D)/scripts/dtc/libfdt \
+			-L$(@D) -o $(@D)/fdtput \
+			$(@D)/scripts/dtc/util.c \
+			$(@D)/scripts/dtc/fdtput.c \
 		-lfdt;
 	$(TARGET_MAKE_ENV) $(TARGET_CC) $(TARGET_CFLAGS) \
-		-I$(LINUX_DIR)/scripts/dtc -I$(LINUX_DIR)/scripts/dtc/libfdt \
-			-L$(LINUX_DIR)/lib -o $(LINUX_DIR)/lib/fdtget \
-			$(LINUX_DIR)/scripts/dtc/util.c \
-			$(LINUX_DIR)/scripts/dtc/fdtget.c \
+		-I$(@D)/scripts/dtc -I$(@D)/scripts/dtc/libfdt \
+			-L$(@D) -o $(@D)/fdtget \
+			$(@D)/scripts/dtc/util.c \
+			$(@D)/scripts/dtc/fdtget.c \
 		-lfdt;
 endef
 
 define LIBFDT_INSTALL_STAGING_CMDS
-	$(INSTALL) -D $(LINUX_DIR)/lib/libfdt.so $(STAGING_DIR)/lib/libfdt.so
-	$(INSTALL) -D $(LINUX_DIR)/scripts/dtc/libfdt/fdt.h $(STAGING_DIR)/include/fdt.h
-	$(INSTALL) -D $(LINUX_DIR)/scripts/dtc/libfdt/libfdt.h $(STAGING_DIR)/include/libfdt.h
-	$(INSTALL) -D $(LINUX_DIR)/scripts/dtc/libfdt/libfdt_env.h $(STAGING_DIR)/include/libfdt_env.h
+	$(INSTALL) -D $(@D)/libfdt.so $(STAGING_DIR)/lib/libfdt.so
+	$(INSTALL) -D $(@D)/scripts/dtc/libfdt/fdt.h $(STAGING_DIR)/include/fdt.h
+	$(INSTALL) -D $(@D)/scripts/dtc/libfdt/libfdt.h $(STAGING_DIR)/include/libfdt.h
+	$(INSTALL) -D $(@D)/scripts/dtc/libfdt/libfdt_env.h $(STAGING_DIR)/include/libfdt_env.h
 endef
 
 define LIBFDT_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(LINUX_DIR)/lib/libfdt.so $(TARGET_DIR)/lib/libfdt.so
-	$(INSTALL) -D $(LINUX_DIR)/lib/fdtdump $(TARGET_DIR)/bin/fdtdump
-	$(INSTALL) -D $(LINUX_DIR)/lib/fdtput $(TARGET_DIR)/bin/fdtput
-	$(INSTALL) -D $(LINUX_DIR)/lib/fdtget $(TARGET_DIR)/bin/fdtget
+	$(INSTALL) -D $(@D)/libfdt.so $(TARGET_DIR)/lib/libfdt.so
+	$(INSTALL) -D $(@D)/fdtdump   $(TARGET_DIR)/bin/fdtdump
+	$(INSTALL) -D $(@D)/fdtput    $(TARGET_DIR)/bin/fdtput
+	$(INSTALL) -D $(@D)/fdtget    $(TARGET_DIR)/bin/fdtget
 endef
 
 $(eval $(generic-package))
