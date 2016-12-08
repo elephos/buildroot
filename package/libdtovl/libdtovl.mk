7022d052248231b0b34c55830cbf7284f2c51b3b
Joe Watkins <krakjoe@php.net>
2016-12-08 08:14:13 +0000

    fix dep chain

--- package/libdtovl/libdtovl.mk
+++ package/libdtovl/libdtovl.mk
@@ -8,7 +8,7 @@ LIBDTOVL_SITE = $(call github,elephos,libdtovl,$(LIBDTOVL_VERSION))
 LIBDTOVL_INSTALL_STAGING = YES
 LIBDTOVL_INSTALL_TARGET = YES
 LIBDTOVL_CONF_OPTS = -DCMAKE_INSTALL_PREFIX=/usr
-LIBDTOVL_DEPENDENCIES = linux host-pkgconf
+LIBDTOVL_DEPENDENCIES = libfdt host-pkgconf
 
 $(eval $(cmake-package))
 
